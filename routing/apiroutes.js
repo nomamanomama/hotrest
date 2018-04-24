82636f1787ad48c0733caae5dbea1e9dc97892c0
nomamanomama <33500937+nomamanomama@users.noreply.github.com>
2018-04-23 19:42:21 -0500

    basic routes

---
+++ routing/apiroutes.js
@@ -0,0 +1,21 @@
+var path = require('path');
+
+module.exports = function (app) {
+
+    app.get('/app/tables', function (req, res) {
+        
+    })
+
+    app.get('/app/wait', function (req, res) {
+        
+    })
+
+    app.post('/app/wait', function (req, res) {
+
+    })
+
+    app.post('/app/tables', function (req, res) {
+
+    })
+
+}
