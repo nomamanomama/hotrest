0a4def16219bebae4c27c5fe99270902fdee5fa3
nomamanomama <33500937+nomamanomama@users.noreply.github.com>
2018-04-23 20:14:08 -0500

    added public static folder

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+app.use(express.static('./public'));
+
 require('./routing/apiroutes.js')(app);
 require('./routing/htmlroutes.js')(app);
 
