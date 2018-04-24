e6d197f4f58cbf91ed5c62508976fd0798c153d4
nomamanomama <33500937+nomamanomama@users.noreply.github.com>
2018-04-23 21:24:39 -0500

    updated api routes

--- routing/apiroutes.js
+++ routing/apiroutes.js
@@ -1,32 +1,39 @@
 var tables = require('../app/data/tables.js');
 var wait = require('../app/data/wait.js');
-
+console.log(tables);
+console.log(wait);
 var path = require('path');
 
 module.exports = function (app) {
 
     app.get('/app/tables', function (req, res) {
         console.log(req.body);
-        tables.push(req.body);
-        res.end();
+        
+        res.json(tables);
     })
 
     app.get('/app/wait', function (req, res) {
         console.log(req.body);
-        wait.push(req.body);
-        res.end();
+        res.json(wait);
     })
 
-    app.post('/app/wait', function (req, res) {
-
-    })
 
     app.post('/app/tables', function (req, res) {
-
+        if(tables.length < 5){
+            tables.push(req.body);
+            res.json(true);
+        } else {
+            wait.push(req.body);
+            res.json(true);
+        }
+        
     })
 
     app.post('/app/clear', function(req, res) {
+        tables = [];
+        wait = [];
 
+        console.log(tables);
     })
 
 }
